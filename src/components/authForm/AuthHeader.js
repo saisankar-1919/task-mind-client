d2582573c32867fc63a9ce9ab7f6890390960d59
saisankar <101086279+saisankar-1919@users.noreply.github.com>
2025-02-17 23:05:32 +0530

    text change

--- src/components/authForm/AuthHeader.js
+++ src/components/authForm/AuthHeader.js
@@ -5,7 +5,7 @@ export const AuthHeader = ({ type }) => {
     <AppBar position="static" sx={{ bgcolor: "#4B0082", borderRadius: 2 }}>
       <Toolbar>
         <Typography variant="h5" fontWeight={600} color="white">
-          {type === "login" ? "Welcome back!" : "Register"}
+          {type === "login" ? "Welcome back to the app!" : "Register"}
         </Typography>
       </Toolbar>
     </AppBar>
